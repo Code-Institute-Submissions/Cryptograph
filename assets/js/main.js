289ac260aff2862a140c022e82cacebfb5a28c16
Wayne Pegg <peggy161152@gmail.com>
2019-10-25 15:10:28 +0100

    update to ask tutor question

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,9 @@
-var xhr = new XMLHttpRequest();
-
-xhr.open("GET", "https://api.cryptoapis.io/v1/exchanges/asset/5b1ea92e584bf50020130612");
-xhr.send();
-
-xhr.onreadystatechange = function() {
-    if(this.readystate == 4 && this.status == 200) {
-        document.getElementById('data').innerHTML = this.responseText;
-        console.log("success");
-    }
-};
-//JQuery activated when DOM ready to be manipulated
-$(document).ready(function() {
-    $()
-});
+queue()
+  .defer(d3.csv, "data/BTCData.csv")
+  .await(makeDataViz);
+  
+console.log("Just before the makeData function");
+function makeDataViz(error, coinData) {
+  console.log("Just inside the makeData function");
+}
+console.log("Just outside the makeData function");
