00c85060d9f5f3cf4c558a95f9ece3f58df280a0
Wayne Pegg <peggy161152@gmail.com>
2019-11-05 14:09:23 +0000

    Graphs updated and Scatter Plot/trader education

--- assets/js/dataViz.js
+++ assets/js/dataViz.js
@@ -8,25 +8,35 @@ if (typeof(f.top) != "undefined") {f=f.top(Infinity);}else{}
 if (typeof(f.dimension) != "undefined") {f=f.dimension(function(d) { return "";}).top(Infinity);}else{}
 console.log(filter+"("+f.length+") = "+JSON.stringify(f).replace("[","[\n\t").replace(/}\,/g,"},\n\t").replace("]","\n]"));
 }
+
 //Formats the data from the .csv file so that Date can be used at a JS Date object
 var formatData = data.forEach(function(d){
     var tempDate = new Date(d.Date);
     d.Date = tempDate;
-    console.log(tempDate);
-});
+    });
+
+//User-defined data
+const numOfBTC = 1;
+const dateOfPurchase = new Date("2018-04-01");
+const costPerCoin = 4000; //Amount per coin in $
+const roiTarget = 350; //Target profit level - 350% ROI of initial value
 
 //Create a crossfilter object
 var ndx = crossfilter(data);
 
-//var typeDimension = data.dimension(function(d){return d.type;});
 var dateDimension = ndx.dimension(function(d){ return d.Date;});
-//print_filter('dateDimension');
-var dateGroup = dateDimension.group();
-print_filter('dateGroup.reduceSum(function(d){return d.Close})');
-
+var dateGroup = dateDimension.group().reduceSum(function(d){return d.High});
 var maxDate = dateDimension.top(1)[0].Date;
 var minDate = dateDimension.bottom(1)[0].Date;
-console.log('data.groupAll.reduceValue.value: ' + ndx.groupAll().reduceCount().value());//row count and default function of CF
+
+var volGroup = dateDimension.group().reduceSum(function(d){return d.Volume_BTC;});
+
+var profitDateDimension = dateDimension.filterRange([dateOfPurchase, maxDate]);
+var profitGroup = profitDateDimension.group().reduceSum(function(d){
+                    return Math.floor( ((d.High - costPerCoin) / costPerCoin)  * 100 )});
+
+var scatterDimension = ndx.dimension(function(d){ return[d.Volume_BTC, d.High];});
+var scatterGroup = scatterDimension.group();
 
 var coinOverview = dc.lineChart("#data")
                     .width(1200)
@@ -35,34 +45,82 @@ var coinOverview = dc.lineChart("#data")
                     .dimension(dateDimension)
                     .group(dateGroup)
                     .renderArea(true)
+                    .yAxisLabel("Value/$")
                     .x(d3.scaleTime().domain([minDate ,maxDate]))
-                    .colors(d3.schemePastel1)
+                    .colors("#1c9099")
                     .renderHorizontalGridLines(true)
                     .useViewBoxResizing(true);
                     dc.renderAll();
-//var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-//var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 var dataTable = dc.dataTable("#table")
-                .width(400)
+                .width(500)
                 .height(400)
                 .dimension(dateDimension)
                 .group(function(d){ return d;})
                 .showGroups(false)
                 .order(d3.descending)
-                .size(3)
+                .size(5)
                 .columns([{label:'Date', format:function(d){
-                    return d.Date;}}, 
+                    return d.Date.toDateString();}}, 
                         'Open', 
                         'Close', 
                         'High', 
                         'Low'])
-                .useViewBoxResizing(true)
-                .on("renderlet", function(table){
-                    table.selectAll('.dc-table-group').classed('info', true);
-                });
+                .useViewBoxResizing(true);
 
                 dc.renderAll();
 
+var profitGraph = dc.lineChart('#profitData')
+                    .height(300)
+                    .width(500)
+                    .brushOn(false)
+                    .dimension(dateDimension)
+                    .group(profitGroup)
+                    .x(d3.scaleTime().domain([dateOfPurchase, maxDate]).nice())
+                    .colors("#756bb1")
+                    .y(d3.scaleLinear().domain([-100, 400]))
+                    .yAxisLabel("Profit levels/%")
+                    .renderArea(true)
+                    .renderHorizontalGridLines(true)
+                    .xyTipsOn(true)
+                    .useViewBoxResizing(true);
+    
+                    profitGraph.xAxis().ticks(4);
+                    dc.renderAll();
+
+var volGraph = dc.barChart("#tradeVolume")
+                    .height(300)
+                    .width(500)
+                    .brushOn(false)
+                    .rangeChart(coinOverview)
+                    .margins({top:50,bottom:50,right:50,left:50})
+                    .dimension(dateDimension)
+                    .group(volGroup)
+                    .x(d3.scaleTime().domain([minDate ,maxDate]).nice())
+                    .colors("#91cf60")
+                    .yAxisLabel("Volume of BTC/unit coin")
+                    .renderHorizontalGridLines(true)
+                    .useViewBoxResizing(true);
+
+                    volGraph.xAxis().ticks(4);
+                    
+                    dc.renderAll();
+
+var volHighScatter = dc.scatterPlot('#volHighScatter')
+                    .height(600)
+                    .width(600)
+                    .margins({top:50,bottom:50,right:50,left:50})
+                    .dimension(scatterDimension)
+                    .group(scatterGroup)
+                    .x(d3.scaleLinear().domain([0, 130000]))
+                    .y(d3.scaleLinear().domain([0, 20000]))
+                    .xAxisLabel("Volume of BTC traded/unit coin")
+                    .yAxisLabel("Intraday High Value/$ per coin")
+                    .renderHorizontalGridLines(true)
+                    .useViewBoxResizing(true);
+
+                    volHighScatter.xAxis().ticks(6);
+                    dc.renderAll();
+                    print_filter(scatterGroup);
 
 });//End const data
